e1de18db4fd58ba39c105141718fc21ff98ab9a4
kamalexe <83895579+kamalexe@users.noreply.github.com>
2022-10-19 20:31:42 +0530

    Full View Blog

--- irrigreatapp/irrigreatappurls.py
+++ irrigreatapp/irrigreatappurls.py
@@ -3,17 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.bloghome, name='bloghome'),
+    path('timeLine', views.timeLine, name='timeLine'),
     path('smRegView', views.smRegView, name='smRegView'),
     path('smReg', views.smReg, name='smReg'),
     path('smLoginView', views.smLoginView, name='smLoginView'),
     path('smLogin', views.smLogin, name='smLogin'),
     path('smProfile', views.smProfile, name='smProfile'),
     path('smLogout', views.smLogout, name='smLogout'),
+    path('post_Comment', views.post_Comment, name='post_Comment'),
     path('friendprofile/<str:id>', views.friendprofile, name='friendprofile'),
+    path('<str:id>', views.friendprofile, name='friendprofile'),
     path('follow', views.follow, name='follow'),
-    path('friendprofile/follow/<str:id>', views.follow, name='follow'),
+    path('follow/<str:id>', views.follow, name='follow'),
     path('createPost/<str:id>', views.createPost, name='createPost'),
     path('like/<str:id>', views.likepost, name='like'),
     path('deletePost/<str:id>', views.deletePost, name='deletePost'),
-    path('postComment', views.postComment, name='postComment'),
+    path('fullBlog/<str:id>', views.fullBlog, name='fullBlog'),
 ]
